91a31fb62e9e5c73355c784cef073fb62cdec612
Adam Chamberlin <adam@limegreenpickle.com>
2022-12-15 11:31:24 +0000

    block theme you say?

---
+++ wp-content/themes/adamchamberlin/patterns/page-home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Sample home page.
+ * Slug: adamc/page-home
+ * Categories: featured
+ * Viewport Width: 1280
+ */
+?>
+<!-- wp:pattern {"slug":"adamc/general-hero-one-column"} /-->
+<!-- wp:pattern {"slug":"adamc/general-hero-two-columns-contrast-background"} /-->
+<!-- wp:pattern {"slug":"adamc/general-hero-two-columns"} /-->
+<!-- wp:pattern {"slug":"adamc/general-cta-button-contrast-background"} /-->
+<!-- wp:pattern {"slug":"adamc/general-hero-three-columns"} /-->
