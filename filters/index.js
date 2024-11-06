4f8e4809fe44ae263bf8e2e1f6fa13ee2c29b7a6
Adam Chamberlin <adam@mackman.co.uk>
2024-11-06 16:59:45 +0000

    still need to fix the excerpts and meta description

--- filters/index.js
+++ filters/index.js
@@ -5,7 +5,7 @@ import metadata from "../data/metadata.js";
 export default function (eleventyConfig) {
   eleventyConfig.addFilter("excerpt", (post) => {
     const content = post.replace(/(<([^>]+)>)/gi, "");
-    return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
+    return content.substr(0, content.lastIndexOf(" ", 400)) + "...";
   });
 
   eleventyConfig.addFilter("splitlines", function (input) {
