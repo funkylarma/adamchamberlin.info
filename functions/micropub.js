b7a64509e12e0f4d2bbf4b040d544ddd377d595e
Adam Chamberlin <adam@mackman.co.uk>
2024-12-19 19:13:40 +0000

    testing some imports via micropub

--- functions/micropub.js
+++ functions/micropub.js
@@ -1,13 +1,63 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+import { Octokit } from "@octokit/rest"
+import { Buffer } from 'node:buffer'
+
+export async function onRequest(context) {
+
+  // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+  const octokit = new Octokit({ auth: context.env.GITHUB_ACCESS_TOKEN });
+
+  // Get the body contents
+  const body = await context.request.json();
+
+  // Check we have an h-entry element
+  if (body.type == 'h-entry') {
+
+    // Get the properties
+    const data = body.properties
+
+    // Check it is a checkin
+    if (data.category[0] === "checkin") {
+
+      // Grab the checkin details
+      const checkin = body.properties.checkin[0]
+
+      // The format is a bit weird,
+      // where title and content are array values with a single entry
+      const title = checkin.properties.name[0]
+      const date = data.published[0]
+      const lat = checkin.properties.latitude[0]
+      const lng = checkin.properties.longitude[0]
+      const url = checkin.properties.url[0]
+      const filename = new Date().valueOf()
+      const path = "/src/checkins/"
+
+      var fileContent = []
+
+      fileContent.push("---")
+      fileContent.push('date: ' + date)
+      fileContent.push('title: ' + title)
+      fileContent.push('latitude: ' + lat)
+      fileContent.push('longitude: ' + lng)
+      fileContent.push('url: ' + url)
+      fileContent.push('category: checkin')
+      fileContent.push('---')
+
+      const {
+        updated,
+        data: { commit },
+      } = await octokit.repos.createOrUpdateFileContents({
+        owner: "funkylarma",
+        repo: "adamchamberlin.info",
+        path: "src/checkins/" + filename + '.md',
+        content: Buffer.from(fileContent.join("\n")).toString("base64"),
+        message: "Import checkin from Swarm: " + filename,
+      });
+      return new Response(`Imported Swarm activity`);
+    }
+  } else {
+    return new Response(`no joy`);
+  }
+
+  return new Response(`Hello world`);
 }
 
-// exports.handler = (event, context, callback) => {
-//   // GET request, we can return an empty JSON here
-//   if (event.httpMethod === 'GET') {
-//     return callback(null, {
-//       statusCode: 200,
-//       body: "{}"
-//     })
-//   }
-// }
--- src/data/keys.js
+++ src/data/keys.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 
 const indexNow = process.env.INDEX_NOW_KEY ? process.env.INDEX_NOW_KEY : 'dummy';
+const mapBox = process.env.MAP_BOX_KEY ? process.env.MAP_BOX_KEY : 'dummy';
 
 export default {
   indexNow: indexNow,
+  mapBox: mapBox,
 };
