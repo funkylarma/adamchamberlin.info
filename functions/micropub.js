a979a2a6c883959c5f03c3f7b74d7d4befcf9921
Adam Chamberlin <adam@limegreenpickle.com>
2024-12-21 11:46:30 +0000

    more error checking

--- functions/micropub.js
+++ functions/micropub.js
@@ -12,52 +12,71 @@ export async function onRequestPost(context) {
 
     // Check if we have a category
     if (entry.category != null) {
-      // Is it a check in
-      if (entry.category[0] === 'checkin') {
-        // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-        const octokit = new Octokit({ auth: context.env.GITHUB_ACCESS_TOKEN });
+      // if we only have one category
+      if (entry.category.length == 1) {
+        // Check is is a single checkin
+        if (entry.category[0] === 'checkin') {
+          // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+          const octokit = new Octokit({ auth: context.env.GITHUB_ACCESS_TOKEN });
 
-        // Grab the checkin details
-        const checkin = entry.checkin[0];
+          // Grab the checkin details
+          const checkin = entry.checkin[0];
 
-        // The format is a bit weird,
-        // where title and content are array values with a single entry
-        const title = checkin.properties.name[0];
-        const date = data.published[0];
-        const lat = checkin.properties.latitude[0];
-        const lng = checkin.properties.longitude[0];
-        const url = checkin.properties.url[0];
-        const filename = new Date().valueOf();
-        const path = '/src/checkins/';
+          console.log(checkin);
 
-        var fileContent = [];
+          // The format is a bit weird,
+          // where title and content are array values with a single entry
+          const title = checkin.properties.name[0];
+          const date = entry.published[0];
+          const lat = checkin.properties.latitude[0];
+          const lng = checkin.properties.longitude[0];
+          const url = checkin.properties.url[0];
+          const filename = new Date().valueOf();
+          const path = '/src/checkins/';
 
-        fileContent.push('---');
-        fileContent.push('date: ' + date);
-        fileContent.push('title: ' + title);
-        fileContent.push('latitude: ' + lat);
-        fileContent.push('longitude: ' + lng);
-        fileContent.push('url: ' + url);
-        fileContent.push('categories:');
-        fileContent.push(' - checkin');
-        fileContent.push('---');
+          var fileContent = [];
 
-        const contents = fileContent.join('\n').toString('base64');
+          fileContent.push('---');
+          fileContent.push('date: ' + date);
+          fileContent.push('title: ' + title);
+          fileContent.push('latitude: ' + lat);
+          fileContent.push('longitude: ' + lng);
+          fileContent.push('url: ' + url);
+          fileContent.push('categories:');
+          fileContent.push(' - checkin');
+          fileContent.push('---');
 
-        const {
-          updated,
-          data: { commit },
-        } = await octokit.repos.createOrUpdateFileContents({
-          owner: 'funkylarma',
-          repo: 'adamchamberlin.info',
-          path: 'src/checkins/' + filename + '.md',
-          content: btoa(contents),
-          message: 'Import checkin from Swarm: ' + filename,
+          const contents = fileContent.join('\n').toString('base64');
+
+          const {
+            updated,
+            data: { commit },
+          } = await octokit.repos.createOrUpdateFileContents({
+            owner: 'funkylarma',
+            repo: 'adamchamberlin.info',
+            path: 'src/checkins/' + filename + '.md',
+            content: btoa(contents),
+            message: 'Import checkin from Swarm: ' + filename,
+          });
+          return Response.json(
+            { message: 'Imported Swarm activity' },
+            { status: 201, headers: { Location: 'https://adamchamberlin.info' } }
+          );
+        } else {
+          return Response.json(
+            {
+              message: 'No entry method found for this category',
+            },
+            {
+              status: 400,
+            }
+          );
+        }
+      } else {
+        // Loop over the categories
+        entry.category.forEach(function (category, index) {
+          console.log(category, index);
         });
-        return Response.json(
-          { message: 'Imported Swarm activity' },
-          { status: 201, headers: { Location: 'https://adamchamberlin.info' } }
-        );
       }
     }
 
