016b2aa1e485156fb5cd19895793173582355085
Adam Chamberlin <adam@limegreenpickle.com>
2024-12-28 12:44:28 +0000

    broken the opengraph images

--- utils/events.js
+++ utils/events.js
@@ -5,20 +5,25 @@ export default {
   afterBuild: function () {
     const ogiDir = './src/assets/ogi/';
     fs.readdir(ogiDir, function (err, files) {
-      if (files.length > 0) {
-        files.forEach(function (filename) {
-          if (filename.endsWith('.svg')) {
-            let imageUrl = ogiDir + filename;
-            Image(imageUrl, {
-              formats: ['jpeg'],
-              outputDir: './_site/assets/ogi/',
-              filenameFormat: function (id, src, width, format, options) {
-                let outputFilename = filename.substring(0, filename.length - 4);
-                return `${outputFilename}.${format}`;
-              },
-            });
-          }
-        });
+      if (err) console.log(err);
+      else {
+        console.log('\nCurrent directory filenames:');
+        if (files.length > 0) {
+          files.forEach(function (filename) {
+            console.log(filename);
+            if (filename.endsWith('.svg')) {
+              let imageUrl = ogiDir + filename;
+              Image(imageUrl, {
+                formats: ['jpeg'],
+                outputDir: './_site/assets/ogi/',
+                filenameFormat: function (id, src, width, format, options) {
+                  let outputFilename = filename.substring(0, filename.length - 4);
+                  return `${outputFilename}.${format}`;
+                },
+              });
+            }
+          });
+        }
       }
     });
   },
