654da1a2f6e8ea7d0948e793bb91f270ea94749e
Adam Chamberlin <adam@limegreenpickle.com>
2025-01-18 11:56:16 +0000

    complete rewrite of the checkin import

--- functions/micropub.js
+++ functions/micropub.js
@@ -7,123 +7,83 @@ export async function onRequestPost(context) {
   // Get the body contents
   const request = await context.request.json();
 
+  // Init a micropub object
+  let micropub = {
+    frontMatterContent: [],
+    bodyContent: [],
+  };
+
   // Check we have an h-entry element
   if (request.type == "h-entry") {
     // Get the entry properties
     const entry = request.properties;
 
-    console.log("Request properties: ");
-    console.log(entry);
+    // Start the frontmatter content
+    micropub.frontMatterContent.push("---");
 
-    // Check if we have a category
-    if (entry.category != null) {
-      // if we only have one category
-      if (entry.category.length == 1) {
-        console.log("There is only one category");
-        // Check is is a single checkin
-        if (entry.category[0] === "checkin") {
-          // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-          const octokit = new Octokit({
-            auth: context.env.GITHUB_ACCESS_TOKEN,
-          });
-
-          // Grab the checkin details
-          const checkin = entry.checkin[0];
-
-          console.log(checkin);
-
-          // The format is a bit weird,
-          // where title and content are array values with a single entry
-          const title = checkin.properties.name[0];
-          const date = entry.published[0];
-          const lat = checkin.properties.latitude[0];
-          const lng = checkin.properties.longitude[0];
-          const url = checkin.properties.url[0];
-          const filename = new Date().valueOf();
-          const path = "/src/notes/checkins/";
-
-          if (entry.content[0] !== undefined) {
-          }
-
-          var fileContent = [];
-
-          fileContent.push("---");
-          fileContent.push("date: " + date);
-          fileContent.push("title: " + title);
-          fileContent.push("latitude: " + lat);
-          fileContent.push("longitude: " + lng);
-          fileContent.push("url: " + url);
-          fileContent.push("category: checkin");
-          fileContent.push("---");
-
-          // If we have a checkin post
-          if (entry.content[0] !== undefined) {
-            fileContent.push("");
-            fileContent.push(entry.content[0]);
-          }
-
-          // If we have a checkin photo
-          if (entry.photo[0] !== undefined) {
-            fileContent.push("");
-            fileContent.push(
-              '<img src="' + entry.photo[0] + '" alt="Checkin Photo" />'
-            );
-          }
-
-          const contents = fileContent.join("\n").toString("base64");
-
-          // const {
-          //   updated,
-          //   data: { commit },
-          // } = await octokit.repos.createOrUpdateFileContents({
-          //   owner: 'funkylarma',
-          //   repo: 'adamchamberlin.info',
-          //   path: 'src/notes/checkins/' + filename + '.md',
-          //   content: btoa(contents),
-          //   message: 'Import checkin from Swarm: ' + filename,
-          // });
-
-          return Response.json(
-            { message: "Imported Swarm activity" },
-            {
-              status: 201,
-              headers: { Location: "https://adamchamberlin.info" },
-            }
-          );
-        } else {
-          return Response.json(
-            {
-              message: "No entry method found for this category",
-            },
-            {
-              status: 400,
-            }
-          );
-        }
-      } else {
-        // Loop over the categories
-        entry.category.forEach(function (category, index) {
-          console.log(category, index);
-        });
-      }
+    // Give it a publish date
+    if (entry.published) {
+      // Set the date
+      micropub.date = entry.published[0];
     }
 
-    // Check if we have a content element
-    if (entry.content != null) {
+    // Define some possible content types
+    const content_types = [
+      "rsvp",
+      "in-reply-to",
+      "repost-of",
+      "like-of",
+      "listen-of",
+      "watch-of",
+      "bookmark-of",
+      "ate",
+      "drank",
+      "photo",
+      "checkin",
+    ];
+
+    // Loop through the content types and check if we have a match
+    content_types.forEach((type) => {
+      if (type in entry) {
+        switch (type) {
+          case "checkin":
+            processCheckin(entry.checkin);
+            break;
+          case "photo":
+            processPhoto(entry.photo);
+        }
+      }
+    });
+
+    // If we have a name then it is an article
+    if (entry.name) {
+      micropub.frontMatterContent.push("category: article");
+    }
+
+    // Check if we have any tags
+    if (entry.category) {
+      micropub.frontMatterContent.push("tags:");
+      processTags(entry.category);
+    }
+
+    // End the front matter
+    micropub.frontMatterContent.push("---");
+    micropub.frontMatterContent.push("");
+
+    if (entry.content) {
+      micropub.bodyContent.push(entry.content[0]);
+      micropub.bodyContent.push("");
+    } else {
       return Response.json(
         { message: entry.content },
         { status: 201, headers: { Location: "https://adamchamberlin.info" } }
       );
     }
 
-    return Response.json(
-      {
-        message: "No entry method found",
-      },
-      {
-        status: 400,
-      }
-    );
+    // Commit the micropub
+    const responseObj = commitMicropub();
+
+    return responseObj;
   } else {
     return Response.json(
       {
@@ -133,7 +93,62 @@ export async function onRequestPost(context) {
     );
   }
 
-  function processCheckin(request) {}
+  function processCheckin(checkin) {
+    // Where should we save the checkin
+    micropub.path = "/src/notes/checkins/";
+    // Give it a filename
+    micropub.filename = new Date().valueOf();
+    (micropub.message = "Import checkin from Swarm: " + micropub.filename),
+      micropub.frontMatterContent.push("date: " + micropub.date);
+    micropub.frontMatterContent.push("title: " + checkin[0].properties.name);
+    micropub.frontMatterContent.push(
+      "latitude: " + checkin[0].properties.latitude
+    );
+    micropub.frontMatterContent.push(
+      "longitude: " + checkin[0].properties.longitude
+    );
+    micropub.frontMatterContent.push("url: " + checkin[0].properties.url);
+    micropub.frontMatterContent.push("category: checkin");
+  }
+
+  function processPhoto(photos) {
+    photos.forEach((photo) => {
+      micropub.bodyContent.push(
+        '<img src="' + photo + '" alt="Checkin Photo" />'
+      );
+      micropub.bodyContent.push("");
+    });
+  }
+
+  function processTags(tags) {
+    tags.forEach((tag) => {
+      micropub.frontMatterContent.push(" - " + tag);
+    });
+  }
+
+  async function commitMicropub() {
+    let contents = micropub.frontMatterContent.join("\n");
+    contents += micropub.bodyContent.join("\n").toString("base64");
+
+    const {
+      updated,
+      data: { commit },
+    } = await octokit.repos.createOrUpdateFileContents({
+      owner: "funkylarma",
+      repo: "adamchamberlin.info",
+      path: micropub.path + micropub.filename + ".md",
+      content: btoa(contents),
+      message: micropub.message,
+    });
+
+    return Response.json(
+      { message: micropub.message },
+      {
+        status: 201,
+        headers: { Location: "https://adamchamberlin.info" },
+      }
+    );
+  }
 
   return responseObj;
 }
