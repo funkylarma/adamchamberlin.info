926f04ba98db75c634804cc7ccc7a98bdc2d8e8d
Adam Chamberlin <adam@limegreenpickle.com>
2025-01-19 15:05:47 +0000

    adding an auth check to the micropub

--- functions/micropub.js
+++ functions/micropub.js
@@ -1,9 +1,33 @@
 /** @format */
 
-import { Octokit } from "@octokit/rest";
+import { Octokit } from '@octokit/rest';
+
+// Get requests
+export async function onRequest(context) {
+  const token = context.request.headers.get('Authorization');
+
+  if (!token) {
+    return new Response('No token provided', { status: 401 });
+  }
+
+  return Response.json(
+    {
+      message: 'Hello Micropub world',
+    },
+    { status: 200 }
+  );
+}
 
 // Post context
 export async function onRequestPost(context) {
+  // Get the token, if there is any
+  const token = context.request.headers.get('Authorization');
+
+  // Check that we have a token
+  if (!token) {
+    return new Response('No token provided', { status: 401 });
+  }
+
   // Get the body contents
   const request = await context.request.json();
 
@@ -14,42 +38,44 @@ export async function onRequestPost(context) {
   };
 
   // Check we have an h-entry element
-  if (request.type == "h-entry") {
+  if (request.type == 'h-entry') {
     // Get the entry properties
     const entry = request.properties;
 
     // Start the frontmatter content
-    micropub.frontMatterContent.push("---");
+    micropub.frontMatterContent.push('---');
 
     // Give it a publish date
     if (entry.published) {
       // Set the date
       micropub.date = entry.published[0];
+    } else {
+      micropub.date = Date.now();
     }
 
     // Define some possible content types
     const content_types = [
-      "rsvp",
-      "in-reply-to",
-      "repost-of",
-      "like-of",
-      "listen-of",
-      "watch-of",
-      "bookmark-of",
-      "ate",
-      "drank",
-      "photo",
-      "checkin",
+      'rsvp',
+      'in-reply-to',
+      'repost-of',
+      'like-of',
+      'listen-of',
+      'watch-of',
+      'bookmark-of',
+      'ate',
+      'drank',
+      'photo',
+      'checkin',
     ];
 
     // Loop through the content types and check if we have a match
     content_types.forEach((type) => {
       if (type in entry) {
         switch (type) {
-          case "checkin":
+          case 'checkin':
             processCheckin(entry.checkin);
             break;
-          case "photo":
+          case 'photo':
             processPhoto(entry.photo);
         }
       }
@@ -57,22 +83,22 @@ export async function onRequestPost(context) {
 
     // If we have a name then it is an article
     if (entry.name) {
-      micropub.frontMatterContent.push("category: article");
+      micropub.frontMatterContent.push('category: article');
     }
 
     // Check if we have any tags
     if (entry.category) {
-      micropub.frontMatterContent.push("tags:");
+      micropub.frontMatterContent.push('tags:');
       processTags(entry.category);
     }
 
     // End the front matter
-    micropub.frontMatterContent.push("---");
-    micropub.frontMatterContent.push("");
+    micropub.frontMatterContent.push('---');
+    micropub.frontMatterContent.push('');
 
     if (entry.content) {
       micropub.bodyContent.push(entry.content[0]);
-      micropub.bodyContent.push("");
+      micropub.bodyContent.push('');
     }
 
     // Commit the micropub
@@ -81,7 +107,7 @@ export async function onRequestPost(context) {
   } else {
     return Response.json(
       {
-        message: "Not a valid micropub post, missing h-entry element",
+        message: 'Not a valid micropub post, missing h-entry element',
       },
       { status: 400 }
     );
@@ -89,20 +115,20 @@ export async function onRequestPost(context) {
 
   function processCheckin(checkin) {
     // Where should we save the checkin
-    micropub.path = "src/notes/checkins/";
+    micropub.path = 'src/notes/checkins/';
     // Give it a filename
     micropub.filename = new Date().valueOf();
-    (micropub.message = "Import checkin from Swarm: " + micropub.filename),
-      micropub.frontMatterContent.push("date: " + micropub.date);
-    micropub.frontMatterContent.push("title: " + checkin[0].properties.name);
+    (micropub.message = 'Import checkin from Swarm: ' + micropub.filename),
+      micropub.frontMatterContent.push('date: ' + micropub.date);
+    micropub.frontMatterContent.push('title: ' + checkin[0].properties.name);
     micropub.frontMatterContent.push(
-      "latitude: " + checkin[0].properties.latitude
+      'latitude: ' + checkin[0].properties.latitude
     );
     micropub.frontMatterContent.push(
-      "longitude: " + checkin[0].properties.longitude
+      'longitude: ' + checkin[0].properties.longitude
     );
-    micropub.frontMatterContent.push("url: " + checkin[0].properties.url);
-    micropub.frontMatterContent.push("category: checkin");
+    micropub.frontMatterContent.push('url: ' + checkin[0].properties.url);
+    micropub.frontMatterContent.push('category: checkin');
   }
 
   function processPhoto(photos) {
@@ -110,13 +136,13 @@ export async function onRequestPost(context) {
       micropub.bodyContent.push(
         '<img src="' + photo + '" alt="Checkin Photo" />'
       );
-      micropub.bodyContent.push("");
+      micropub.bodyContent.push('');
     });
   }
 
   function processTags(tags) {
     tags.forEach((tag) => {
-      micropub.frontMatterContent.push(" - " + tag);
+      micropub.frontMatterContent.push(' - ' + tag);
     });
   }
 
@@ -124,20 +150,20 @@ export async function onRequestPost(context) {
     // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
     const octokit = new Octokit({ auth: context.env.GITHUB_ACCESS_TOKEN });
 
-    let contents = micropub.frontMatterContent.join("\n");
-    contents += micropub.bodyContent.join("\n").toString("base64");
+    let contents = micropub.frontMatterContent.join('\n');
+    contents += micropub.bodyContent.join('\n').toString('base64');
 
     try {
       const { data } = await octokit.repos.createOrUpdateFileContents({
-        owner: "funkylarma",
-        repo: "adamchamberlin.info",
-        path: micropub.path + micropub.filename + ".md",
+        owner: 'funkylarma',
+        repo: 'adamchamberlin.info',
+        path: micropub.path + micropub.filename + '.md',
         content: btoa(contents),
         message: micropub.message,
       });
       return Response.json(
         { message: data.commit.message },
-        { status: 201, headers: { Location: "https://adamchamberlin.info" } }
+        { status: 201, headers: { Location: 'https://adamchamberlin.info' } }
       );
     } catch (err) {
       return Response.json(
@@ -151,17 +177,7 @@ export async function onRequestPost(context) {
 
   return Response.json(
     {
-      message: "Hello Micropub world",
-    },
-    { status: 200 }
-  );
-}
-
-// On all requests
-export async function onRequest(context) {
-  return Response.json(
-    {
-      message: "Hello Micropub world",
+      message: 'Hello Micropub world',
     },
     { status: 200 }
   );
