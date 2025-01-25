986749b333bafd8efccfb9f6c6d8b9abf9fd4fdd
Adam Chamberlin <adam@limegreenpickle.com>
2025-01-25 15:51:30 +0000

    moved books to HardCover

--- data/books.js
+++ data/books.js
@@ -1,50 +1,127 @@
 import EleventyFetch from '@11ty/eleventy-fetch';
-import parser from 'fast-xml-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const TOKEN = 'Bearer ' + process.env.HARDCOVER_KEY;
 
 export default async function () {
-  console.log('Fetching Oku book data');
-
-  const url = 'https://oku.club/rss/collection/sNsDk';
-  const fetchConfig = {
-    duration: '12h',
-    type: 'text',
-  };
-
-  let response;
-
+  console.log( 'Fetching Hardcover book data' );
+  
+  let currentlyReading = {};
+  let wantToRead = {};
+  
+  // Currently Reading
   try {
-    response = await EleventyFetch(url, fetchConfig);
-  } catch (e) {
-    console.error(`Fetch failed in books.js. ${e}`);
+    
+    const currentlyReadingResponse = await EleventyFetch( 'https://api.hardcover.app/v1/graphql', {
+      duration: '12h',
+      type: 'json',
+      fetchOptions: {
+        headers: {
+          'content-type': 'application/json',
+          authorization: TOKEN,
+        },
+        method: 'POST',
+        body: JSON.stringify( {
+          query: `
+          {
+            me {
+              user_books(where: {status_id: {_eq: 2}}) {
+                book {
+                  title
+                  slug
+                  image {
+                    url
+                  }
+                  contributions {
+                    author {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }`,
+        } ),
+      }
+    } );
+    const booklist = currentlyReadingResponse.data.me[ 0 ][ 'user_books' ].map( book => {
+      let transformedBook = {};
+      //transformedBook.date = new Date( book.pubDate );
+      transformedBook.url = 'https://hardcover.app/books/' + book.book.slug;
+      transformedBook.data = {
+        title: book.book.title,
+        author: book.book.contributions[ 0 ].author.name,
+        cover: book.book.image.url,
+        category: 'book',
+      };
+      return transformedBook;
+      
+    } );
+    
+    currentlyReading = booklist;
+    
+  } catch ( e ) {
+    console.log( e.status, e.statusText );
+    console.error( `Failed to fetch the currently reading in books.js` );
     return `It has failed: ${e}`;
   }
-
-  let feed;
-  const result = parser.XMLValidator.validate(response);
-
-  if (result === true) {
-    const xmlparser = new parser.XMLParser();
-    feed = xmlparser.parse(response);
-  } else {
-    console.error(`rss.js - XML is invalid. Reason: ${result.err.msg}`);
+  
+  try {
+    const wantToReadResponse = await EleventyFetch( 'https://api.hardcover.app/v1/graphql', {
+      duration: '12h',
+      type: 'json',
+      fetchOptions: {
+        headers: {
+          'content-type': 'application/json',
+          authorization: TOKEN,
+        },
+        method: 'POST',
+        body: JSON.stringify( {
+          query: `
+          {
+            me {
+              user_books(where: {status_id: {_eq: 1}}) {
+                book {
+                  title
+                  slug
+                  image {
+                    url
+                  }
+                  contributions {
+                    author {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }`,
+        } ),
+      }
+    } );
+    const booklist = wantToReadResponse.data.me[ 0 ][ 'user_books' ].map( book => {
+      let transformedBook = {};
+      //transformedBook.date = new Date( book.pubDate );
+      transformedBook.url = 'https://hardcover.app/books/' + book.book.slug;
+      transformedBook.data = {
+        title: book.book.title,
+        author: book.book.contributions[ 0 ].author.name,
+        //cover: book.image.url,
+        category: 'book',
+      };
+      return transformedBook;
+      
+    } );
+    wantToRead = booklist;
+  } catch ( e ) {
+    console.log( e.status, e.statusText );
+    console.error( `Failed to fetch the want to read in books.js` );
+    return `It has failed: ${e}`;
   }
-
-  let readingList = feed.rss.channel.item;
-
-  let books = readingList.map((book) => {
-    let transformedBook = {};
-    transformedBook.date = new Date(book.pubDate);
-    transformedBook.url = book.guid;
-    transformedBook.data = {
-      title: book.title,
-      author: book['dc:creator'],
-      cover: book['oku:cover'],
-      category: 'book',
-    };
-    return transformedBook;
-  });
-
+  
   return {
-    currentlyReading: books,
+    currentlyReading: currentlyReading,
+    wantToRead: wantToRead,
   };
 }
