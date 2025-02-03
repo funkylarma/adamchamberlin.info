940dc48c8f6a643759d1d93ff5a9a90fcf8dee3e
Adam Chamberlin <adam@mackman.co.uk>
2025-02-03 13:42:41 +0000

    updating the media format

--- src/media/media.11tydata.js
+++ src/media/media.11tydata.js
@@ -2,7 +2,10 @@ export default {
   eleventyComputed: {
     // Modify the permalink
     permalink: ( data ) => {
-      return `/${data.page.filePathStem.replace('/media', '/')}/`;
+      const d = new Date( data.page.date );
+      const year = d.getFullYear();
+      const month = d.getMonth() + 1;
+      return `/${year}/${month}/${data.page.filePathStem.replace('/media', '/')}/`;
     },
     
     eleventyExcludeFromCollections: ( data ) => {
