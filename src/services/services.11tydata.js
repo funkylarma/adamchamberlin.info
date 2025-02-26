91728ea0e8673b4be3fdb1654a8641fa26cfa305
Adam Chamberlin <adam@limegreenpickle.com>
2025-02-26 19:43:45 +0000

    adding services

---
+++ src/services/services.11tydata.js
@@ -0,0 +1,36 @@
+/**
+ * If a post is a `draft`, it is for dev mode only.
+ *
+ * @param {object} data Post data
+ * @param {boolean} [data.draft=false] Post draft status
+ * @returns {boolean}
+ */
+function devOnly ( data ) {
+  return Boolean( data.draft );
+}
+
+export default {
+  eleventyComputed: {
+    // Modify the permalink
+    permalink: ( data ) => {
+      if ( process.env.ELEVENTY_ENV === 'production' && devOnly( data ) ) {
+        return false;
+      }
+      return `/${data.page.filePathStem.replace('/services', '/')}/`;
+    },
+    
+    eleventyExcludeFromCollections: ( data ) => {
+      if ( process.env.ELEVENTY_ENV === 'production' && devOnly( data ) ) {
+        return true;
+      }
+      return false;
+    },
+  },
+  
+  // Set the tag for collections
+  tags: [ 'note' ],
+  
+  // What layout to use
+  layout: 'note.liquid',
+  
+};
