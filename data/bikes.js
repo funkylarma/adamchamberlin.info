f740c2d79fcfc0bcec99a1680f8b6f3b8c6c8af6
Adam Chamberlin <adam@mackman.co.uk>
2025-03-11 09:26:31 +0000

    fixing an image issue on the notes archive

---
+++ data/bikes.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Bike',
+  bikes: [
+    {
+      name: 'Prince',
+      make: 'Pinarello',
+      model: 'Prince FX',
+      url: 'prince',
+    },
+    {
+      name: 'Crosshill',
+      make: 'LaPierre',
+      model: 'Crosshill 5',
+      url: 'crosshill',
+    }
+  ],
+}
